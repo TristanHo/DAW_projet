5b118a17bf0ff0f4a03f1bdd83f22c29f90c2e46
Zoryko <164355196+Zoryko@users.noreply.github.com>
2024-03-21 15:21:36 +0100

    Add files via upload

--- view/users/list.php
+++ view/users/list.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Liste des utilisateurs</title>
+    <title>Formulaire Question</title>
 </head>
 <body>
 
@@ -15,7 +15,12 @@
         }
 
     ?>
+    <br><br> <input type="button" id="theme" value="Mode nuit"/>
 </form>
 
 </body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script  type="text/javascript" src="../js/theme.js"></script>
+
 </html>
