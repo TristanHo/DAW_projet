7bfc40524619694e5c3d3aef4ca67cf5e2ea2f04
Zoryko <164355196+Zoryko@users.noreply.github.com>
2024-04-18 17:00:09 +0200

    Add files via upload

--- config/routeur.php
+++ config/routeur.php
@@ -6,11 +6,7 @@ spl_autoload_register(function ($controller) {
     require_once('../controller/'.$controller.'.php');
 });
 
-
-/*
-PARTIE CONNEXION, CREATION DE COMPTE, GESTION D'UTILISATEUR
-*/
-if(isset($_POST['action']) && !is_null($_POST['action'])){
+if(isset($_POST['action']) && !is_null($_POST['action'])) {
     $action = $_POST['action'];
 
     switch($action) {
@@ -49,63 +45,56 @@ if(isset($_POST['action']) && !is_null($_POST['action'])){
                 ControllerUser::creerCompte();
                 break;
             }
+
+        case 'createCours' :
+            {
+                if(isset($_POST['nom'])) {
+                    ControllerCours::insertCours($_COOKIE['login'], $_POST['nom']);
+                    break;
+                }
+            }
+
+        case 'modifCours' :
+            {
+                if(isset($_POST['nom']) && isset($_POST['id'])) {
+                    ControllerCours::modifCours($_COOKIE['login'], $_POST['nom'], $_POST['id']);
+                }
+                break;
+            }
+
+        case 'deleteCours' :
+            {
+                ControllerCours::deleteCours($_POST['id']);
+                break;
+            }
     }
 
 $_POST['action'] = null;
 }
 
-else if(isset($_GET['action']) && !is_null($_GET['action'])){
-    $action = $_GET['action'];
+/*
+    PARTIE FORUM
+    */
+    if(isset($_GET['messageInput']))
+    {
+        /* 
+        A REMPLACER PAR COOKIE
+        A REMPLACER PAR COOKIE
+        A REMPLACER PAR COOKIE
+        */
+        $_SESSION['login'] = "famous_singer";
+        /* 
+        A REMPLACER PAR COOKIE
+        A REMPLACER PAR COOKIE
+        A REMPLACER PAR COOKIE
+        */
 
-    switch($action) {
-        case 'disconnect' :
-            {
-                ControllerUser::deconnexion();
-                break;
-            }
 
+        require_once 'ControllerForum.php'; ControllerForum::addMessage();
     }
 
-
-$_GET['action'] = null;
-}
-
-
-/*
-PARTIE DECONNEXION
-*/
-if(isset($_POST['action']) && !is_null($_POST['action'])){
-    $actionConnexion = $_POST['action'];
-    switch ($actionConnexion){
-        case 'connect' :  require_once 'ControllerUser.php';ControllerUser::connect();break;
-        case 'creerCompte' :  require_once 'ControllerUser.php';ControllerUser::creerCompte();break;
-    } 
-};
-if(isset($_GET['action']) && !is_null($_GET['action'])){
-    $action = $_GET['action'];
-}
-
-
-/*
-PARTIE FORUM
-*/
-if(isset($_POST['messageInput']))
-{
-    require_once '../controller/ControllerForum.php'; ControllerForum::addMessage($_GET['topic_id'], $_GET['topic_title']);
-}
-
-if(isset($_POST['btnDeleteMessage']) && isset($_GET['id_message']))
-{
-    require_once '../controller/ControllerForum.php'; ControllerForum::removeMessage($_GET['id_message']);
-}
-
-if(isset($_POST['btnDeleteTopic']))
-{
-    require_once '../controller/ControllerForum.php'; ControllerForum::removeTopic($_GET['id_cours']);
-}
-
-if(isset($_POST['topicInput']))
-{
-    require_once '../controller/ControllerForum.php'; ControllerForum::addTopic();
-}
+    if(isset($_POST['btnDeleteMessage']) && isset($_GET['id_message']))
+    {
+        require_once 'ControllerForum.php'; ControllerForum::removeMessage($_GET['id_message']);
+    }
 ?>
